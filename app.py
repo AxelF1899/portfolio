8af839ddb5edb939f405a8ad7ee76a3234d6587a
Axel FloB <fl385309@uaeh.edu.mx>
2025-02-27 23:21:48 -0600

    portfolio 1st version

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', active_page='portfolio')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html', active_page='resume')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', active_page='about')
+
+if __name__ == '__main__':
+    app.run(debug=True)
